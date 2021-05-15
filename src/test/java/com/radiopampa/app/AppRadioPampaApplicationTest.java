9251eb089aded0de1b5e104bc4971e74a6857cb9
Gustavo <gviglesias@alumnos.unsada.edu.ar>
2021-05-15 20:09:59 -0300

    deploy4

--- src/test/java/com/radiopampa/app/AppRadioPampaApplicationTest.java
+++ src/test/java/com/radiopampa/app/AppRadioPampaApplicationTest.java
@@ -3,7 +3,8 @@ package com.radiopampa.app;
 
 
 import org.junit.jupiter.api.Test;
-
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
 class AppRadioPampaApplicationTest {
 
 	@Test
