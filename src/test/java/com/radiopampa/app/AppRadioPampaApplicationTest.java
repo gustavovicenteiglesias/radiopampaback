724b8f3427cf97af5de5b2f178c17a46844f9cad
Gustavo <gviglesias@alumnos.unsada.edu.ar>
2021-05-15 15:13:44 -0300

    deploy

--- src/test/java/com/radiopampa/app/AppRadioPampaApplicationTest.java
+++ src/test/java/com/radiopampa/app/AppRadioPampaApplicationTest.java
@@ -1,14 +1,13 @@
 package com.radiopampa.app;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 import org.junit.jupiter.api.Test;
 
 class AppRadioPampaApplicationTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void contextLoads() {
 	}
 
 }
